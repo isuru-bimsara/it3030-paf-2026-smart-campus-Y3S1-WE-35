67bab8ba4a140e5439a8151c0ecd89165e064ac6
Dilshan221 <dunith0773239462@gmail.com>
2026-04-23 20:50:26 +0530

    delete some files

--- Uni/src/main/java/com/smart/Uni/exception/BookingConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.smart.Uni.exception;
-
-public class BookingConflictException extends RuntimeException {
-    public BookingConflictException(String message) {
-        super(message);
-    }
-}
