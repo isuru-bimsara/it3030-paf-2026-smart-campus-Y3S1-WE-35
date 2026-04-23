d4841ca8863236620937d41853da9b7c8814858f
Dilshan221 <dunith0773239462@gmail.com>
2026-04-24 00:25:57 +0530

    fgg

---
+++ Uni/src/main/java/com/smart/Uni/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package com.smart.Uni.repository;
+
+import com.smart.Uni.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+    List<Notification> findByUserIdAndReadFalse(Long userId);
+    long countByUserIdAndReadFalse(Long userId);
+    Optional<Notification> findByIdAndUserId(Long id, Long userId);
+}
