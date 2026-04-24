3be1e508527e7c29f9069bec8f3ea290826624a8
Nimna Prabhashwara <nimnaprabashwara555@gmail.com>
2026-04-24 12:50:11 +0530

    comment responese

---
+++ Uni/src/main/java/com/smart/Uni/dto/response/CommentResponse.java
@@ -0,0 +1,17 @@
+package com.smart.Uni.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class CommentResponse {
+    private Long id;
+    private Long ticketId;
+    private Long userId;
+    private String userName;
+    private String userRole;
+    private String content;
+    private LocalDateTime createdAt;
+}
